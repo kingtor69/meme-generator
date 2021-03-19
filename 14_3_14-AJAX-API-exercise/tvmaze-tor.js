7650bc7058d8489d019dd1b5a545fbec9047a6d0
Tor Kingdon <tor@hearkitty.com>
2021-03-19 02:31:49 -0600

    finished. error messages still need some work to be Bristol.

--- 14_3_14-AJAX-API-exercise/tvmaze-tor.js
+++ 14_3_14-AJAX-API-exercise/tvmaze-tor.js
@@ -17,7 +17,6 @@ async function searchShows(query) {
                 image
             });
         }
-        console.log(returnArrOfObjs);
         return returnArrOfObjs;
     } catch (err) {
         displayShowError(err);
@@ -28,19 +27,18 @@ async function searchShows(query) {
 function populateShows(shows) {
     const $showsList = $("#shows-list");
     $showsList.empty();
-    episodeButtons.splice(0, episodeButtons.length);
 
     for (let show of shows) {
         const episodesId = `${show.id}-episodes-div`;
         const episodesUlId = `${show.id}-episodes-list`;
-        let $item = $(`
+        let $showCard = $(`
         <div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
           <div class="card" data-show-id="${show.id}">
             <div class="card-body">
               <img class="card-img-top" src=${show.image} alt="Card image cap">
                 <h5 class="card-title">${show.name}</h5>
                 <p class="card-text">${show.summary}</p>
-                <button class="cart-text btn btn-block btn-secondary episode-buttons" id="${show.id}">see episode list</button>
+                <button class="cart-text btn btn-block btn-secondary episode-buttons" data-show-name="${show.name}" id="${show.id}">see episode list</button>
             </div>
           </div>
         </div>
@@ -54,20 +52,20 @@ function populateShows(shows) {
           </div>
         </div>
       `);
-        $showsList.append($item);
-        episodeButtons.unshift(document.getElementById(show.id));
+        $(`#${show.id}`).data("name", show.name);
+        $showsList.append($showCard);
     }
 }
 
 // TODO -- I don't think this works
 function displayShowError(err) {
     console.log('show error ', err)
-    let $item = $(
+    // TODO: might want to make more user-friendly error message(s)
+    let $showCard = $(
         `<div class="col-md-6 col-lg-3 Show" data-show-id="">
            <div class="card" data-show-id="">
              <div class="card-body">
                <h5 class="card-title">Error</h5>
-                // TODO: might want to make more user-friendly error message(s)
                <p class="card-text">${err}</p>
              </div>
            </div>
@@ -93,51 +91,33 @@ async function getEpisodes(id) {
             const name = episode.name;
             const season = episode.season;
             const number = episode.number;
-            episodesOutput.unshift({
+            episodesOutput.push({
                 id,
                 name,
                 season,
                 number
             });
         }
-        episodesOutput.unshift(id);
+        // episodesOutput.unshift(id);
         return episodesOutput;
     } catch (err) {
         displayEpisodeError(err);
-        return [`error ${err}`];
     }
-    // TODO: return array-of-episode-info, as described in docstring above
-    // I think this means I need to change the individual buttons to one get episodes button
-    // and put the episode lists under their respective shows
 }
 
-// TODO: fix that to reflect changes above
-// come to think of it, it never worked at all, so 
-// either fix it or scrap and restart it now that you 
-// have a better understanding
-function displayEpisodes(episodes) {
-    console.log('display episodes?');
+function displayEpisodes(showName, episodes) {
     try {
-        console.log(episodes);
-        console.log(episodes.length);
-        // debugger;
-        const showUlId = `${episodes[0]}-episodes-list`;
-        const episodesUl = document.getElementById(showUlId);
-        for (let i = 1; i < episodes.length; i++) {
-            console.log(`loop of i=${i}`);
-            console.log(episodes[i]);
-            const liOfI = document.createElement('li');
-            liOfI.id = episode.id;
-            liOfI.innerText = `${episode.name} (season ${episode.season}, number ${episode.number})`;
-            episodesUl.appendChild(liOfI);
-            //       episodesUl.innerHTML += `
-            //   <li ID="${episode.id}">${episode.name} (season ${episode.season}, number ${episode.number})</li>
-            // `;
+        const $episodesList = $('#episodes-list');
+        $episodesList.empty();
+        const $episodesHeader = $(`<h3>${showName}</h3>`);
+        $episodesList.prepend($episodesHeader);
+        for (let episode of episodes) {
+            const $episodeLi = $(`<li>${episode.name} (season ${episode.season}, number ${episode.number})</li>`);
+            $episodesList.append($episodeLi);
         }
-        episodesUl.classList.remove(Hide);
-        episodesUl.classList.add(Show);
+        $('#episodes-area').show();
     } catch (err) {
-        return `episode display error ${err}`;
+        displayEpisodeError(err);
     }
 }
 
@@ -145,45 +125,26 @@ function displayEpisodeError(err) {
     console.log('episode error ', err);
 }
 
-// TODO: can probably purify this code even further and get rid of this...
-// in fact, you definitely can -- see notes about making only one
-// episodes button
-const episodeButtons = [];
-
 $("#search-form").on("submit", async function handleSearch(evt) {
     evt.preventDefault();
 
     let query = $("#search-query").val();
     if (!query) return;
 
-    // $("#episodes-area").hide();
+    $("#episodes-area").hide();
 
-    // TODO: error messages still aren't working, but at least I've got it to display the show and not hide everything when there's an error
     let shows = await searchShows(query);
-    if (typeof shows[0] === "string" && shows[0].startsWith("error")) {
-        shows.splice(0, 1);
-        return;
-    } else(populateShows(shows));
+    populateShows(shows);
 
-    // TODO: (see purification idea above) ...by putting a querySelectAll here for the episode buttons
-    if (episodeButtons.length > 0) {
-        for (let button of episodeButtons) {
-            button.addEventListener('click', async function handleEpisodes(evt) {
-                evt.preventDefault();
-                const episodesArr = [];
-                // I think this next line is the root of my problems
-                // maybe I should go back to the way I did the shows 
-                // with putting the button.ids in an array, 
-                // checking [0] for error message,
-                // THEN iterating
-
-                const episodeArray = (await getEpisodes(button.id));
-                // console.log(episodeArray);
-                if (typeof episodeArray[0] === "string" && !episodeArray[0].startsWith("error")) {
-                    // console.log("got some, now let's display them");
-                    displayEpisodes(episodeArray);
-                };
-            });
-        };
+    const $episodeButtons = $('.episode-buttons');
+    for (button of $episodeButtons) {
+        button.addEventListener('click', async function handleEpisodes(evt) {
+            evt.preventDefault();
+            const clickedId = evt.path[0].id;
+            let episodes = await getEpisodes(clickedId);
+            const $buttonOfId = $(`#${clickedId}`);
+            const showName = $buttonOfId.data();
+            displayEpisodes(showName.showName, episodes);
+        })
     };
 });
